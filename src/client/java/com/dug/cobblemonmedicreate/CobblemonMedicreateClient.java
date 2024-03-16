13933b7a9d2e5b0d7ccb39820f1ac6c2adfbbe36
Dugchugy <61024715+Dugchugy@users.noreply.github.com>
2024-03-16 13:58:36 -0700

    Added mod template from template generator

---
+++ src/client/java/com/dug/cobblemonmedicreate/CobblemonMedicreateClient.java
@@ -0,0 +1,10 @@
+package com.dug.cobblemonmedicreate;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CobblemonMedicreateClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
