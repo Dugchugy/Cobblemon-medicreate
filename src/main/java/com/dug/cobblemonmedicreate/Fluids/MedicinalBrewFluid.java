d03c7ea309b862b02b0728c1a7f9034796403e0c
Dugchugy <61024715+Dugchugy@users.noreply.github.com>
2024-03-18 16:48:14 -0700

    Added Still medicinal brew and function to add to registry

--- src/main/java/com/dug/cobblemonmedicreate/Fluids/MedicinalBrewFluid.java
+++ src/main/java/com/dug/cobblemonmedicreate/Fluids/MedicinalBrewFluid.java
@@ -4,9 +4,14 @@ import net.minecraft.block.BlockState;
 import net.minecraft.fluid.FlowableFluid;
 import net.minecraft.fluid.Fluid;
 import net.minecraft.fluid.FluidState;
+import net.minecraft.item.BucketItem;
 import net.minecraft.item.Item;
+import net.minecraft.item.Items;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.Properties;
+import net.minecraft.util.Identifier;
 
 public abstract class MedicinalBrewFluid extends MediFluidBase{
 
@@ -69,6 +74,30 @@ public abstract class MedicinalBrewFluid extends MediFluidBase{
 			return false;
 		}
 	}
-    
+
+    public static class Still extends MedicinalBrewFluid {
+        @Override
+        public int getLevel(FluidState fluidState) {
+            return 8;
+        }
+ 
+        @Override
+        public boolean isStill(FluidState fluidState) {
+            return true;
+        }
+    }
+
+    /**
+     * A function that calls the required registy functions to register the fluid
+     */
+    public static void Register(){
+        STILL = Registry.register(Registries.FLUID, new Identifier("cobblemon-medicreate", "medicinal-brew"), 
+        new MedicinalBrewFluid.Still());
+        FLOW = Registry.register(Registries.FLUID, new Identifier("cobblemon-medicreate", "medicinal-brew-flow"), 
+        new MedicinalBrewFluid.Flowing());
+        BUCKET = Registry.register(Registries.ITEM, new Identifier("cobblemon-medicreate", "medicinal-brew-bucket"), 
+        new BucketItem(STILL, new Item.Settings().recipeRemainder(Items.BUCKET).maxCount(1)));
+    }
+
 
 }
