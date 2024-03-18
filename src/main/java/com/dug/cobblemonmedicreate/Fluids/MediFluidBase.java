a4a616258067a50f309e0e4cbbacef303d3cee92
Dugchugy <61024715+Dugchugy@users.noreply.github.com>
2024-03-18 16:17:02 -0700

    Added MediFluidBase as base fluid class
    added fluid following fluid tutorial on fabric site

---
+++ src/main/java/com/dug/cobblemonmedicreate/Fluids/MediFluidBase.java
@@ -0,0 +1,83 @@
+package com.dug.cobblemonmedicreate.Fluids;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.fluid.*;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.world.*;
+
+public abstract class MediFluidBase extends FlowableFluid {
+    /**
+     * @return wether the given fluid matches this fluid
+     */
+    @Override
+	public boolean matchesType(Fluid fluid) {
+		return fluid == getStill() || fluid == getFlowing();
+	}
+
+    /**
+	 * @return whether the fluid is infinite (which means can be infinitely created like water). In vanilla, it depends on the game rule.
+	 */
+	/*@Override
+	protected boolean isInfinite() {
+		return false;
+	}*/
+
+    /**
+	 * Perform actions when the fluid flows into a replaceable block. Water drops
+	 * the block's loot table. Lava plays the "block.lava.extinguish" sound.
+	 */
+	@Override
+	protected void beforeBreakingBlock(WorldAccess world, BlockPos pos, BlockState state) {
+		final BlockEntity blockEntity = state.hasBlockEntity() ? world.getBlockEntity(pos) : null;
+		Block.dropStacks(state, world, pos, blockEntity);
+	}
+
+    /**
+	 * Lava returns true if it's FluidState is above a certain height and the
+	 * Fluid is Water.
+	 * 
+	 * @return whether the given Fluid can flow into this FluidState
+	 */
+	@Override
+	protected boolean canBeReplacedWith(FluidState fluidState, BlockView blockView, BlockPos blockPos, Fluid fluid, Direction direction) {
+		return false;
+	}
+
+    /**
+	 * Possibly related to the distance checks for flowing into nearby holes?
+	 * Water returns 4. Lava returns 2 in the Overworld and 4 in the Nether.
+	 */
+	@Override
+	protected int getFlowSpeed(WorldView worldView) {
+		return 4;
+	}
+ 
+	/**
+	 * Water returns 1. Lava returns 2 in the Overworld and 1 in the Nether.
+	 */
+	@Override
+	protected int getLevelDecreasePerBlock(WorldView worldView) {
+		return 1;
+	}
+ 
+	/**
+	 * Water returns 5. Lava returns 30 in the Overworld and 10 in the Nether.
+	 */
+	@Override
+	public int getTickRate(WorldView worldView) {
+		return 5;
+	}
+ 
+	/**
+	 * Water and Lava both return 100.0F.
+	 */
+	@Override
+	protected float getBlastResistance() {
+		return 100.0F;
+	}
+
+    
+}
