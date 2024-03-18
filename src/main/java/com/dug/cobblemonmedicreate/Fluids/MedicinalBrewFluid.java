7a2d96849eeb1dee72ad7230d68fc789f184944d
Dugchugy <61024715+Dugchugy@users.noreply.github.com>
2024-03-18 16:31:56 -0700

    Added bas fluid functions from tutorial

--- src/main/java/com/dug/cobblemonmedicreate/Fluids/MedicinalBrewFluid.java
+++ src/main/java/com/dug/cobblemonmedicreate/Fluids/MedicinalBrewFluid.java
@@ -1,7 +1,11 @@
 package com.dug.cobblemonmedicreate.Fluids;
 
+import net.minecraft.block.BlockState;
 import net.minecraft.fluid.FlowableFluid;
+import net.minecraft.fluid.Fluid;
+import net.minecraft.fluid.FluidState;
 import net.minecraft.item.Item;
+import net.minecraft.state.property.Properties;
 
 public abstract class MedicinalBrewFluid extends MediFluidBase{
 
@@ -17,5 +21,34 @@ public abstract class MedicinalBrewFluid extends MediFluidBase{
      * A static reference to the bucket for this fluid
      */
     public static Item BUCKET;
+
+    /**
+     * @return the still reference for this fluid
+     */
+    @Override
+	public Fluid getStill() {
+		return STILL;
+	}
+ 
+    /**
+     * @return the flowing reference for this fluid
+     */
+	@Override
+	public Fluid getFlowing() {
+		return FLOW;
+	}
+ 
+    /**
+     * @return the bucekt of this fluid
+     */
+	@Override
+	public Item getBucketItem() {
+		return BUCKET;
+	}
+ 
+	@Override
+	protected BlockState toBlockState(FluidState fluidState) {
+		return YOUR_FLUID_BLOCK_HERE.getDefaultState().with(Properties.LEVEL_15, getBlockStateLevel(fluidState));
+	}
 
 }
