277b939d0876aff0a6f65b515f9923978991f48e
Dugchugy <61024715+Dugchugy@users.noreply.github.com>
2024-03-18 16:26:11 -0700

    Added vars for the fluid types of the objects

--- src/main/java/com/dug/cobblemonmedicreate/Fluids/MedicinalBrewFluid.java
+++ src/main/java/com/dug/cobblemonmedicreate/Fluids/MedicinalBrewFluid.java
@@ -1,5 +1,21 @@
 package com.dug.cobblemonmedicreate.Fluids;
 
+import net.minecraft.fluid.FlowableFluid;
+import net.minecraft.item.Item;
+
 public abstract class MedicinalBrewFluid extends MediFluidBase{
+
+    /**
+     * A static refernce to the still version for this fluid
+     */
+    public static FlowableFluid STILL;
+    /**
+     * A static reference to the flowing version for this fluid
+     */
+    public static FlowableFluid FLOW;
+    /**
+     * A static reference to the bucket for this fluid
+     */
+    public static Item BUCKET;
 
 }
