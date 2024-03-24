f59dc1cd4e26bbca03efacbbd1dd4a94dc1fed0c
Dugchugy <61024715+Dugchugy@users.noreply.github.com>
2024-03-23 17:02:32 -0700

    Added render handler for MedicinalBrew renderer

--- src/client/java/com/dug/cobblemonmedicreate/CobblemonMedicreateClient.java
+++ src/client/java/com/dug/cobblemonmedicreate/CobblemonMedicreateClient.java
@@ -1,10 +1,14 @@
 package com.dug.cobblemonmedicreate;
 
+import com.dug.cobblemonmedicreate.Fluids.*;
+
 import net.fabricmc.api.ClientModInitializer;
 
 public class CobblemonMedicreateClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+
+		MedicinalBrewFluid.registerRenderer();
 	}
 }
