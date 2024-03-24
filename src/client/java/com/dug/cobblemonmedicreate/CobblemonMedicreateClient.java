ec80b9fc4d3c15021758a5095b1f264271284488
Dugchugy <61024715+Dugchugy@users.noreply.github.com>
2024-03-23 18:14:26 -0700

    moved medicinal brew renderer to client

--- src/client/java/com/dug/cobblemonmedicreate/CobblemonMedicreateClient.java
+++ src/client/java/com/dug/cobblemonmedicreate/CobblemonMedicreateClient.java
@@ -3,12 +3,19 @@ package com.dug.cobblemonmedicreate;
 import com.dug.cobblemonmedicreate.Fluids.*;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.render.fluid.v1.FluidRenderHandlerRegistry;
+import net.fabricmc.fabric.api.client.render.fluid.v1.SimpleFluidRenderHandler;
+import net.minecraft.util.Identifier;
 
 public class CobblemonMedicreateClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
 
-		MedicinalBrewFluid.registerRenderer();
+		FluidRenderHandlerRegistry.INSTANCE.register(MedicinalBrewFluid.STILL, MedicinalBrewFluid.FLOW, new SimpleFluidRenderHandler(
+				new Identifier("minecraft:block/water_still"),
+				new Identifier("minecraft:block/water_flow"),
+				0x4CC248
+		));
 	}
 }
