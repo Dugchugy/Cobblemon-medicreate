e8d53a49a921da0e22fd847b846414b328200442
Dugchugy <61024715+Dugchugy@users.noreply.github.com>
2024-03-25 19:54:07 -0700

    Added rendering for all fluids

--- src/client/java/com/dug/cobblemonmedicreate/CobblemonMedicreateClient.java
+++ src/client/java/com/dug/cobblemonmedicreate/CobblemonMedicreateClient.java
@@ -15,7 +15,84 @@ public class CobblemonMedicreateClient implements ClientModInitializer {
 		FluidRenderHandlerRegistry.INSTANCE.register(MedicinalBrewFluid.STILL, MedicinalBrewFluid.FLOW, new SimpleFluidRenderHandler(
 				new Identifier("minecraft:block/water_still"),
 				new Identifier("minecraft:block/water_flow"),
-				0x4CC248
+				0x05E3B3
+		));
+
+		FluidRenderHandlerRegistry.INSTANCE.register(PotionFluid.STILL, PotionFluid.FLOW, 
+		new SimpleFluidRenderHandler(
+				new Identifier("minecraft:block/water_still"),
+				new Identifier("minecraft:block/water_flow"),
+				0x9435F2
+		));
+
+		FluidRenderHandlerRegistry.INSTANCE.register(SuperPotionFluid.STILL, SuperPotionFluid.FLOW, 
+		new SimpleFluidRenderHandler(
+				new Identifier("minecraft:block/water_still"),
+				new Identifier("minecraft:block/water_flow"),
+				0xD15D43
+		));
+
+		FluidRenderHandlerRegistry.INSTANCE.register(HyperPotionFluid.STILL, HyperPotionFluid.FLOW, 
+		new SimpleFluidRenderHandler(
+				new Identifier("minecraft:block/water_still"),
+				new Identifier("minecraft:block/water_flow"),
+				0xDB4B87
+		));
+
+		FluidRenderHandlerRegistry.INSTANCE.register(MaxPotionFluid.STILL, MaxPotionFluid.FLOW, 
+		new SimpleFluidRenderHandler(
+				new Identifier("minecraft:block/water_still"),
+				new Identifier("minecraft:block/water_flow"),
+				0x4BC5DE
+		));
+
+		FluidRenderHandlerRegistry.INSTANCE.register(AntidoteFluid.STILL, AntidoteFluid.FLOW, 
+		new SimpleFluidRenderHandler(
+				new Identifier("minecraft:block/water_still"),
+				new Identifier("minecraft:block/water_flow"),
+				0xFFFF00
+		));
+
+		FluidRenderHandlerRegistry.INSTANCE.register(AwakeningFluid.STILL, AwakeningFluid.FLOW, 
+		new SimpleFluidRenderHandler(
+				new Identifier("minecraft:block/water_still"),
+				new Identifier("minecraft:block/water_flow"),
+				0x00FBFF
+		));
+
+		FluidRenderHandlerRegistry.INSTANCE.register(BurnHealFluid.STILL, BurnHealFluid.FLOW, 
+		new SimpleFluidRenderHandler(
+				new Identifier("minecraft:block/water_still"),
+				new Identifier("minecraft:block/water_flow"),
+				0x3DEB7D
+		));
+
+		FluidRenderHandlerRegistry.INSTANCE.register(FullHealFluid.STILL, FullHealFluid.FLOW, 
+		new SimpleFluidRenderHandler(
+				new Identifier("minecraft:block/water_still"),
+				new Identifier("minecraft:block/water_flow"),
+				0xF7DE39
+		));
+
+		FluidRenderHandlerRegistry.INSTANCE.register(FullRestoreFluid.STILL, FullRestoreFluid.FLOW, 
+		new SimpleFluidRenderHandler(
+				new Identifier("minecraft:block/water_still"),
+				new Identifier("minecraft:block/water_flow"),
+				0x91DB4B
+		));
+
+		FluidRenderHandlerRegistry.INSTANCE.register(IceHealFluid.STILL, IceHealFluid.FLOW, 
+		new SimpleFluidRenderHandler(
+				new Identifier("minecraft:block/water_still"),
+				new Identifier("minecraft:block/water_flow"),
+				0xC973B4
+		));
+
+		FluidRenderHandlerRegistry.INSTANCE.register(ParalyzeHealFluid.STILL, ParalyzeHealFluid.FLOW, 
+		new SimpleFluidRenderHandler(
+				new Identifier("minecraft:block/water_still"),
+				new Identifier("minecraft:block/water_flow"),
+				0xEBE013
 		));
 	}
 }
